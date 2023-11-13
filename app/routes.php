bd41273b513e4ae4caf0bd0de1bb30d345b79afa
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-11-12 23:16:12 -0300

    Feat: route to select by product id and update ok

--- app/routes.php
+++ app/routes.php
@@ -47,6 +47,45 @@ return function (App $app) {
     }
   });
 
+  $app->get('/products/{id}', function (Request $request, Response $response, $args) {
+    $productId = $args['id'];
+
+    $sql = "SELECT * FROM products WHERE id = :id";
+
+    try {
+      $db = new Db();
+      $conn = $db->connect();
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(':id', $productId, PDO::PARAM_INT);
+      $stmt->execute();
+
+      $product = $stmt->fetch(PDO::FETCH_OBJ);
+
+      if (!$product) {
+        $response->getBody()->write(json_encode(['error' => 'Produto não encontrado na base']));
+        return $response
+          ->withHeader('content-type', 'application/json')
+          ->withStatus(404);
+      }
+
+      $db = null;
+
+      $response->getBody()->write(json_encode($product));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e) {
+      $error = [
+        'message' => $e->getMessage()
+      ];
+
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
+    }
+  });
+
   $app->post('/products/add', function (Request $request, Response $response, array $args) {
     $data = $request->getParsedBody();
     $name = $data["name"];
@@ -85,10 +124,49 @@ return function (App $app) {
     }
   });
 
-  $app->group('/users', function (Group $group) {
-    $group->get('', ListUsersAction::class);
-    $group->get('/{id}', ViewUserAction::class);
-  });
+  $app->put('/products/{id}', function (Request $request, Response $response, array $args) {
+      $id = $request->getAttribute('id');
+      $data = $request->getParsedBody();
+      $name = $data["name"];
+      $description = $data["description"];
+      $price = $data["price"];
+      $amount = $data["amount"];
 
+      $sql = "UPDATE products SET
+           name = :name,
+           description = :description,
+           price = :price,
+           amount = :amount
+        WHERE id = $id";
 
+      try {
+        $db = new Db();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':amount', $amount);
+
+        $result = $stmt->execute();
+
+        $db = null;
+        echo "Update successful! ";
+        $response->getBody()->write(json_encode($result));
+        return $response
+          ->withHeader('content-type', 'application/json')
+          ->withStatus(200);
+      } catch (PDOException $e) {
+        $error = array(
+          "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+          ->withHeader('content-type', 'application/json')
+          ->withStatus(500);
+      }
+    }
+  );
 };
